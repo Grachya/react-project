462477289fb260743d3b4bae0c2c308fe10b4d4b
Grachya Darbinyan <grachya.darbinyan@kaspersky.com>
2019-05-21 14:02:10 +0300

    Realize css modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import s from './Navbar.module.css'
 
 const Navbar = () => {
-    return <nav className="nav">
-        <div className="item"><a>Profile</a></div>
-        <div className="item"><a>Messages</a></div>
-        <div className="item"><a>News</a></div>
-        <div className="item"><a>Music</a></div>
-        <div className="item"><a>Settings</a></div>
+    return <nav className={s.nav}>
+        <div className={`${s.item} ${s.active}`}><a>Profile</a></div>
+        <div className={`${s.item}`}><a>Messages</a></div>
+        <div className={`${s.item}`}><a>News</a></div>
+        <div className={`${s.item}`}><a>Music</a></div>
+        <div className={`${s.item}`}><a>Settings</a></div>
     </nav>
 }
 
